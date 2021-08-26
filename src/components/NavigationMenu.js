2116d144f81ffea5ba2d59876f63f89d3a5f5804
Maurício Farias <mauricio.farias@palmsoft.com.br>
2021-08-26 16:59:54 -0300

    Adicionado Header estilo Appbar

--- src/components/NavigationMenu.js
+++ src/components/NavigationMenu.js
@@ -1,12 +1,42 @@
 import React, { Component } from "react";
-// import IconBreadcrumbs from "../subcomponents/IconBreadcrumbs";
+import SimpleTabs from "../subcomponents/SimpleTabs";
 
 export default class NavigationMenu extends Component {
+
+    pages = [
+        {
+            label: "Introdução",
+            page: () => (<p>Introducao</p>)
+        },
+        {
+            label: "Formação Acadêmica",
+            page: () => (<p>Formação Academica</p>)
+        },
+        {
+            label: "Licenças e Certificados",
+            page: () => (<p>Licenças e Certificados</p>)
+        },
+        {
+            label: "Experiência Profissional",
+            page: () => (<p>Experiência Profissional</p>)
+        },
+        {
+            label: "Tecnologias Trabalhadas",
+            page: () => (<p>Tecnologias Trabalhadas</p>)
+        },
+        {
+            label: "Projetos Trabalhos",
+            page: () => (<p>Projetos Trabalhados</p>)
+        },
+        {
+            label: "Em estudo",
+            page: () => (<p>Em estudo</p>)
+        }
+    ]
     
     render() {
         return (
-            // <IconBreadcrumbs />
-            <p>TESTE</p>
+            <SimpleTabs items={this.pages} />
         )
     }
 }
---
+++ src/subcomponents/SimpleTabs.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+export default function SimpleTabs(props) {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const {items} = props;
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  function TabContent() {
+    return items[value].page();
+  }
+
+  console.log("Valor: ", value);
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" scrollButtons="on">
+          {items.map((item, index)=>
+            (
+              <Tab 
+              label={item.label} 
+              {...a11yProps(index)}
+              centered
+               />
+            )
+          )}
+        </Tabs>
+      </AppBar>
+      <TabContent />
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: 'white',
+  },
+}));
